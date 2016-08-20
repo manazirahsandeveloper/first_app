089424dd1d9ea36cbcba61ac95ca080a1c410295
M A Manazir Ahsan <manazirahsan.developer@gmail.com>
2016-08-20 12:45:50 +0800

    Send html content from controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def home
-  	render html: "Bismillah"
+  	render text: "<html><center><h1>Bismillahir Rahmanir Rahim</h1></center></html>"
   end
 end
